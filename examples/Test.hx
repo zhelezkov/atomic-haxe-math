36331177f30e5add28861c61361c87546b729e56
rsredsq <rsredsq@gmail.com>
2015-08-28 01:44:20 +0300

    Initial release!

---
+++ examples/Test.hx
@@ -0,0 +1,12 @@
+
+import com.rsredsq.math.*;
+
+class Test {
+    public static inline function main():Void {
+        var vec:Vec2 = new Vec2();
+        vec.set(1, 0);
+        var vec2:Vec2 = Vec2.fromValues(0, 1);
+        vec += vec2;
+        trace(vec);
+    }
+}
